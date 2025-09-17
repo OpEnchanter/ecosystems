3ef5a85534bedfb8d72adda2246c7c4cad6a1a9e
OpEnchanter <openchanter@gmail.com>
2025-09-17 13:39:08 -0400

    add simple population plotting with python and update readme

---
+++ plot/python/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("../../data/out/log.csv")
+
+print(data)
+
+plt.figure(figsize=(10,6))
+plt.plot(data["Time"], data["Foxes"], label="Foxes")
+plt.plot(data["Time"], data["Rabbits"], label="Rabbits")
+plt.xlabel("Time")
+plt.ylabel("Population")
+plt.title("Organism populations")
+plt.legend()
+plt.grid()
+
+plt.show()
